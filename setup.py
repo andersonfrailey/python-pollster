0eb7a9b41b5aec6b1cd66606a116cafdbba9169c
Adam Hooper <adam@adamhooper.com>
2017-01-12 16:34:30 -0500

    Rewrite for v2

--- setup.py
+++ setup.py
@@ -1,23 +1,30 @@
-from distutils.core import setup
-import pollster
+# coding: utf-8
+
+import sys
+from setuptools import setup, find_packages
+
+NAME = "pollster"
+VERSION = "2.0.0"
+
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil", "pandas >= 0.19.1"]
 
 setup(
-    name='Pollster',
-    version=pollster.__version__,
-    packages=['pollster',],
-    license='BSD',
-    author='Aaron Bycoffe',
-    author_email='bycoffe@huffingtonpost.com',
-    description="A Python wrapper for the Pollster API",
-    long_description="A Python wrapper for the Pollster API",
-    url='https://github.com/huffpostdata/python-pollster',
-    classifiers=[
-                 'Intended Audience :: Developers',
-                 'License :: OSI Approved :: BSD License',
-                 'Natural Language :: English',
-                 'Operating System :: OS Independent',
-                 'Programming Language :: Python',
-                 'Topic :: Software Development :: Libraries :: Python Modules',
-                 ],
-
+    name=NAME,
+    version=VERSION,
+    description="Pollster API",
+    author_email="Adam Hooper <adam.hooper@huffingtonpost.com>",
+    url="https://github.com/huffpostdata/python-pollster",
+    keywords=["Pollster API"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="""Download election-related polling data from Pollster."""
 )
+
